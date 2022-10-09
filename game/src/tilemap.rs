ad299bad21e4a936655e496263f533fe6c120255
Brian Lucas <brianlucas@pop-os.localdomain>
2022-10-09 16:18:33 -0400

    Now generates flooring of the room

--- game/src/tilemap.rs
+++ game/src/tilemap.rs
@@ -5,7 +5,7 @@ const TITLE: &str = "Tiling";
 const WIN_W: f32 = 1280.;
 const WIN_H: f32 = 720.;
 
-const TILE_SIZE: f32 = 45.;
+const TILE_SIZE: f32 = 50.;
 
 #[derive(Component)]
 struct WallTile;
@@ -27,20 +27,26 @@ fn create_random_room(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let wall_handle = asset_server.load("WallTiles.png");
+    let wall_handle = asset_server.load("BrickWall2.png");
 	let wall_atlas = TextureAtlas::from_grid(wall_handle, Vec2::splat(TILE_SIZE), 1, 1);
 	let wall_atlas_len = wall_atlas.textures.len();
 	let wall_atlas_handle = texture_atlases.add(wall_atlas);
 
-    let floor_handle = asset_server.load("WoodFloors.png");
+    let floor_handle = asset_server.load("WoodFloors2.png");
 	let floor_atlas = TextureAtlas::from_grid(floor_handle, Vec2::splat(TILE_SIZE), 2, 1);
 	let floor_atlas_len = floor_atlas.textures.len();
 	let floor_atlas_handle = texture_atlases.add(floor_atlas);
 
     let mut rng = rand::thread_rng();
-    let x_bound = WIN_W/2. - TILE_SIZE/2.;  // Create bounds on where to put in window
-	let y_bound = WIN_H/2. - TILE_SIZE/2.;
-    let size_lower_bound = 6;       // Create bounds on size of room
+
+    // Create bounds on where to put in window
+    let x_bound = WIN_W/4. - TILE_SIZE/2.;  
+	let y_bound = WIN_H/4.;
+    //let x_bound = 10.;  
+	//let y_bound = 10.;
+
+    // Create bounds on size of room
+    let size_lower_bound = 6;       
     let size_upper_bound = 10;     
 
     // Randomly generate dimensions of the room
@@ -142,5 +148,35 @@ fn create_random_room(
             })
             .insert(WallTile);
         y = y - TILE_SIZE;
+    }
+    x = x + TILE_SIZE;
+    y = y + TILE_SIZE;
+    let x_start = x;
+    for i in 0..y_len-1 {
+        let mut count = 1;
+        x = x_start;
+        for i in 0..x_len-1 {
+            let t = Vec3::new(
+                x,
+                y,
+                900.,
+            );
+            commands
+                .spawn_bundle(SpriteSheetBundle {
+                    texture_atlas: floor_atlas_handle.clone(),
+                    transform: Transform {
+                        translation: t,
+                        ..default()
+                    },
+                    sprite: TextureAtlasSprite {
+                        index: i % floor_atlas_len,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .insert(FloorTile);
+            x = x + TILE_SIZE;
+        }
+        y = y + TILE_SIZE;
     }
 }
