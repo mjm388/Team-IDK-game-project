6a2c3066528f09bf09454876cb87e447d4a0aea9
YuQXVI <yuq15@pitt.edu>
2022-11-24 20:53:22 -0500

    Set default json path to the test file

--- src/combat/combat_ai.rs
+++ src/combat/combat_ai.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, fs::File, io::BufReader};
 
 pub fn read_in() -> std::io::Result<HashMap<String, HashMap<String, f64>>>{
-    let f = File::open("agent.json")?;
+    let f = File::open("agent_small.json")?;
     let file = BufReader::new(f);
     let ai_state = serde_json::from_reader(file)?;
     Ok(ai_state)
