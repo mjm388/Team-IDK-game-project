3a435b59782d5cc69086ed7a419aebd9144a0419
mws69 <79333249+mws69@users.noreply.github.com>
2022-11-25 18:00:20 -0500

    l

--- src/strategy/learn/q.rs
+++ src/strategy/learn/q.rs
@@ -17,27 +17,26 @@ impl QLearning {
     }
 }
 
-/*impl<S: State> LearningStrategy<S> for QLearning {
+impl<S: State> LearningStrategy<S> for QLearning {
     fn value(
         &self,
         new_action_values: &Option<&HashMap<S::Act, f64>>,
         current_value: &Option<&f64>,
         reward: f64,
-        init_reward: f64,
         reset: bool,
     ) -> f64 {
         // estimation of max future value
         // the init_reward is used when the state is reset or does not have an existing reward for the next state
         let max_next = 
-            if reset {0} 
+            if reset {&0.} 
             else { 
                 new_action_values
                 .and_then(|m| m.values().max_by(|a, b| a.partial_cmp(b).unwrap()))
-                .unwrap_or(&init_reward)
+                .unwrap_or(&reward)
             };
         // Bellman Equation
         current_value.map_or(reward, |x| {
             x + self.alpha * (reward + self.gamma * max_next - x)
         })
     }
-}*/
+}
